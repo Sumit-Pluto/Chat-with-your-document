99f3ddee9ed3e08c4635ad9a51be07cd07c2e441
Sumit <151113655+Sumit-Pluto@users.noreply.github.com>
2024-06-24 00:55:52 +0530

    Add files via upload

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://res.cloudinary.com/dc3t0ei2e/image/upload/v1713509894/o09w7mwri7ugvqr4luk1.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://res.cloudinary.com/dc3t0ei2e/image/upload/v1713509894/fwk0kvbrh26oukttk3jh.jpg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
